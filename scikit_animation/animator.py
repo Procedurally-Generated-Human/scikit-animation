a4535f769a3bb5028c980abe9990f08fa19dfaa1
Procedurally-Generated-Human <ptoopchinezhad@gmail.com>
2023-04-04 23:21:21 +0330

    added poly regression

--- scikit_animation/animator.py
+++ scikit_animation/animator.py
@@ -1,5 +1,4 @@
-from matplotlib import animation, pyplot as plt
-import numpy as np
+from matplotlib import pyplot as plt
 from sklearn import preprocessing
 from celluloid import Camera
 
@@ -7,63 +6,16 @@ from celluloid import Camera
 
 class Animator:
 
-    def __init__(self, model, x, y, deg=1):
-        self.model = model
-        self.model_copy = model
-        self.x = preprocessing.PolynomialFeatures(degree=deg, include_bias=False).fit_transform(x.reshape(-1, 1))
-        self.XX = preprocessing.PolynomialFeatures(degree=deg, include_bias=False).fit_transform(x)
-        self.y = y
-        self.deg = deg
-        self.scaled_x = preprocessing.StandardScaler().fit(self.x).transform(self.x)
-    
-    def animate(self):
-        fig = plt.figure()
-        self.ax = plt.axes()
-        self.line, = self.ax.plot([], [], lw=3, label="Estimate")
-        fig.legend(loc="upper left")
-        self.animation_init()
-        self.anim = animation.FuncAnimation(fig, self.animation_update,
-                                frames=60, interval=100, blit=True)
-        plt.show()
-
-    def animation_init(self):
-        self.line.set_data(self.x,self.y)
-        return self.line,
-
-    def animation_update(self, i):
-        return self.line,
-
-
-    def save(self, name, fps):
-        self.model = self.model_copy
-        print("Creating Video...")
-        fig = plt.figure()
-        ax = plt.axes()
-        ax.scatter(self.x[:, 0], self.y, color="orange" ,label="Target")
-        fig.legend(loc="upper left")
-        ax.set_title(str(self.deg) + " Poly Regression")
-        self.line, = ax.plot([], [], lw=2, label="Estimate")
-        self.anim = animation.FuncAnimation(fig, self.animation_update, init_func=self.animation_init,
-                                frames=200, interval=100, blit=True)
-        writer = animation.FFMpegWriter(
-        fps=fps, metadata=dict(artist='Parsa Toopchinezhad'), bitrate=100)
-        self.anim.save(name, writer=writer)
-        print("Video saved as", name)
-    
-
-
-
-class Animator2:
-
     def __init__(self, model, x, y, deg=1, speed=1) -> None:
         self.model = model
         self.model_copy = model
         self.x = x
+        self.scaled_x = preprocessing.StandardScaler().fit(self.x).transform(self.x)
         self.y = y
         self.deg = deg
         self.speed = speed
         self.fig, self.ax = plt.subplots()
-        self.camera = Camera(self.fig) 
+        self.camera = Camera(self.fig)
 
     def __animation_init(self):
         pass
--- scikit_animation/sgdregressor_animator.py
+++ scikit_animation/sgdregressor_animator.py
@@ -1,44 +1,25 @@
 from matplotlib import pyplot as plt
 import numpy as np
 from sklearn import preprocessing
-from sklearn.preprocessing import PolynomialFeatures
-from .animator import Animator, Animator2
-from celluloid import Camera
+from .animator import Animator
+
+
 
 
 
 class SGDRegressorAnimator(Animator):
     
-    def animation_init(self):
-        self.line.set_data(self.x,self.y)
-        self.ax.scatter(self.x[:, 0], self.y, color='orange', label="Target")
-        self.ax.set_title(str(self.deg) + " Poly Regression")
-        return self.line,
-
-    def animation_update(self, i):
-        self.model.partial_fit(self.scaled_x,self.y)
-        myline = np.linspace(np.amin(self.x[:, 0]), np.amax(self.x[:, 0]), 1000)
-        poly = PolynomialFeatures(degree=self.deg, include_bias=False)
-        poly_features1 = poly.fit_transform(myline.reshape(-1, 1))
-        scaler = preprocessing.StandardScaler().fit(poly_features1)
-        poly_features1 = scaler.transform(poly_features1)
-        pred = self.model.predict(poly_features1)
-        self.line.set_data(myline, pred)
-        return self.line,
-
-
-
-class SGDRegressorAnimator2(Animator2):
-    
     def __animation_init(self):
         self.ax.set_title(str(self.deg) + " Poly Regression")
 
     def __animation_update(self):
-        self.ax.scatter(self.x,self.y, color='red')
+        self.ax.scatter(self.x[:, 0], self.y, color='red')
         for i in range(10):
-            self.model.partial_fit(self.x,self.y)
-        myline = np.linspace(np.amin(self.x[:, 0]), np.amax(self.x[:, 0]), 10).reshape(-1,1)
-        preds = self.model.predict(myline)
+            self.model.partial_fit(self.scaled_x,self.y)
+        myline = np.linspace(np.amin(self.x[:, 0]), np.amax(self.x[:, 0]), 100).reshape(-1,1)
+        myline_scaled = preprocessing.PolynomialFeatures(degree=self.deg, include_bias=False).fit_transform(myline)
+        myline_scaled = preprocessing.StandardScaler().fit(self.x).transform(myline_scaled)
+        preds = self.model.predict(myline_scaled)
         plt.plot(myline, preds, color='blue')
         self.camera.snap()
 
@@ -47,6 +28,7 @@ class SGDRegressorAnimator2(Animator2):
         self.__animation_init()
         for i in range(1000):
             self.__animation_update()
-        animation = self.camera.animate(interval = 1, repeat = False,
+        animation = self.camera.animate(interval = 1, repeat = True,
                            repeat_delay = 500)
         plt.show()
+
