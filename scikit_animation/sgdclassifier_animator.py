f3ab58bc48b29067f3937089cfca3b56fd349360
Procedurally-Generated-Human <ptoopchinezhad@gmail.com>
2023-04-05 11:19:34 +0330

    added linear classification

--- scikit_animation/sgdclassifier_animator.py
+++ scikit_animation/sgdclassifier_animator.py
@@ -10,24 +10,50 @@ class SGDClassifierAnimator(Animator):
 
     def __animation_init(self):
         self.ax.set_title("Degree "+str(self.deg)+" Polynomial Classification")
+        self.score = 0.0
 
     def __animation_update(self):
-        self.ax.scatter(self.x[:, 0], self.y, color='red')
-        for i in range(10):
-            self.model.partial_fit(self.scaled_x,self.y)
-        myline = np.linspace(np.amin(self.x[:, 0]), np.amax(self.x[:, 0]), 100).reshape(-1,1)
-        myline_scaled = preprocessing.PolynomialFeatures(degree=self.deg, include_bias=False).fit_transform(myline)
-        myline_scaled = preprocessing.StandardScaler().fit(self.x).transform(myline_scaled)
-        preds = self.model.predict(myline_scaled)
-        plt.plot(myline, preds, color='blue')
+        if self.score == 1.0:
+            return
+          
+        labels = np.unique(self.y)
+        self.model.partial_fit(self.x, self.y, labels)
+        self.score = self.model.score(self.x, self.y)
+        
+
+        
+        self.dict = dict.fromkeys(labels)
+        for val in self.dict:
+            self.dict[val] = []
+        for i in range(0,self.x.shape[0]):
+            self.dict[self.y[i]].append(self.x[i])
+        colors = list("rgbcmyk")
+        for x in self.dict.values():
+            x = np.array(x)
+            plt.scatter(x[:, 0],x[:, 1],color=colors.pop())
+        
+        
+        
+        h = .02  # step size in the mesh
+
+        x_min, x_max = self.x[:, 0].min() - 1, self.x[:, 0].max() + 1
+        y_min, y_max = self.x[:, 1].min() - 1, self.x[:, 1].max() + 1
+        xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
+                            np.arange(y_min, y_max, h))
+        Z = self.model.predict(np.c_[xx.ravel(), yy.ravel()])
+
+        # Put the result into a color plot
+        Z = Z.reshape(xx.shape)
+        plt.contour(xx, yy, Z, cmap=plt.cm.Paired)
+        
         self.camera.snap()
 
 
     def animate(self):
         self.__animation_init()
-        for i in range(1000):
+        for i in range(100):
             self.__animation_update()
-        self.animation = self.camera.animate(interval = 1, repeat = True,
+        self.animation = self.camera.animate(interval = 100, repeat = False,
                            repeat_delay = 500)
         plt.show()
 
